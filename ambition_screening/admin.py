c0279fbd21477c6f85f9e1dd1c9946e2c20bb648
Coulson Thabo Kgathi <ckgathi@gmail.com>
2017-05-07 20:23:37 +0200

    add subject screening button

--- ambition_screening/admin.py
+++ ambition_screening/admin.py
@@ -33,3 +33,23 @@ class SubjectScreeningAdmin(ModelAdminMixin, admin.ModelAdmin):
         'medication_contraindicated_with_study_drug': admin.VERTICAL,
         'two_days_amphotericin_b': admin.VERTICAL,
         'two_days_fluconazole': admin.VERTICAL, }
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'report_datetime',
+                'sex',
+                'age',
+                'meningitis_diagoses_by_csf_or_crag',
+                'consent_to_hiv_test',
+                'willing_to_give_informed_consent',
+                'pregnancy_or_lactation',
+                'previous_adverse_drug_reaction',
+                'medication_contraindicated_with_study_drug',
+                'two_days_amphotericin_b',
+                'two_days_fluconazole')
+        }),
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        return (super().get_readonly_fields(request, obj=obj))

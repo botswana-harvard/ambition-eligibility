8840e814046aa10a82bd06933dfba79211086f41
Leslie <leslie.kep@gmail.com>
2017-06-13 16:31:27 +0200

    add form validator class

--- ambition_screening/forms.py
+++ ambition_screening/forms.py
@@ -1,32 +1,31 @@
 from django import forms
 
-from edc_base.modelform_mixins import CommonCleanModelFormMixin
-from edc_base.modelform_validators import FormValidator
+from edc_base.modelform_validators import RequiredFieldValidator
 from edc_constants.constants import YES, FEMALE, NO
 
 from .models import SubjectScreening
 
 
-class SubjectModelFormMixin(CommonCleanModelFormMixin, forms.ModelForm):
-
-    pass
-
-
-class SubjectScreeningForm(SubjectModelFormMixin):
+class SubjectScreeningFormValidator(RequiredFieldValidator):
 
     def clean(self):
-        cleaned_data = super().clean()
-        form_validator = FormValidator(cleaned_data=cleaned_data)
-        condition = cleaned_data.get('gender') == FEMALE
+        condition = self.cleaned_data.get('gender') == FEMALE
         self.required_if_true(
             condition=condition, field_required='pregnancy_or_lactation')
 
-        preg = cleaned_data.get('pregnancy_or_lactation') in [YES, NO]
-        form_validator.required_if_true(
+        preg = self.cleaned_data.get('pregnancy_or_lactation') in [YES, NO]
+        self.required_if_true(
             condition=preg,
             field='pregnancy_or_lactation',
             field_required='preg_test_date')
+        return self.cleaned_data
 
+
+class SubjectScreeningForm(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super().clean()
+        cleaned_data = SubjectScreeningFormValidator(cleaned_data).clean()
         return cleaned_data
 
     class Meta:

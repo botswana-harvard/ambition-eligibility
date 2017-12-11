61ca64062c38ed5a8b143a610d75a2a43af75d30
erikvw <ew2789@gmail.com>
2017-12-11 05:54:50 +0200

    add migrations

--- ambition_screening/models/__init__.py
+++ ambition_screening/models/__init__.py
@@ -1,6 +1,8 @@
+import sys
+
 from django.conf import settings
 
 from .subject_screening import SubjectScreening
 
-if settings.APP_NAME == 'ambition_screening':
+if settings.APP_NAME == 'ambition_screening' and 'makemigrations' not in sys.argv:
     from ..tests import models

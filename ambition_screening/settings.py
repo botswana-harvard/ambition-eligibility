885d81fac69c6747b67b10bec5015dd925beaabd
erikvw <ew2789@gmail.com>
2017-12-28 19:46:25 +0200

    update settings

--- ambition_screening/settings.py
+++ ambition_screening/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'edc_identifier.apps.AppConfig',
     'edc_device.apps.AppConfig',
     'ambition_screening.apps.AppConfig',

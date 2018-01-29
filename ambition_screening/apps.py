ce8404cd817a394ddea82a8ade622295784d6aab
erikvw <ew2789@gmail.com>
2018-01-29 09:18:52 +0200

    add verbose name

--- ambition_screening/apps.py
+++ ambition_screening/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig as DjangoApponfig
 
 class AppConfig(DjangoApponfig):
     name = 'ambition_screening'
+    verbose_name = 'Ambition Subject Screening'
     screening_age_adult_upper = 99
     screening_age_adult_lower = 18
-    include_in_administration_section = False
